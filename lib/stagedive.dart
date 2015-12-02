39eff1fcc8b72749da361b3dcf86c0f53e273119
Mike Mitterer <office@mikemitterer.at>
2015-12-02 11:57:32 +0100

    Removed unnecessary files

--- lib/stagedive.dart
+++ lib/stagedive.dart
@@ -1,28 +1,36 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+/*
+ * Copyright (c) 2015, Michael Mitterer (office@mikemitterer.at),
+ * IT-Consulting and Development Limited.
+ *
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-/// The dasic library.
+/// The stagedive library.
 ///
-/// This is an awesome library. More dartdocs go here.
 library stagedive;
 
 import 'dart:io';
-import 'dart:async';
-import 'dart:collection';
-import 'dart:convert';
 import 'dart:math';
 
+import "package:path/path.dart" as path;
+import "package:yaml/yaml.dart" as yaml;
+import 'package:args/args.dart';
 import 'package:logging/logging.dart';
 import 'package:logging_handlers/logging_handlers_shared.dart';
-
-import 'package:validate/validate.dart';
-import "package:yaml/yaml.dart" as yaml;
-import "package:path/path.dart" as path;
-
 import 'package:pub_cache/pub_cache.dart';
-
-import 'package:intl/intl.dart';
-import 'package:args/args.dart';
+import 'package:validate/validate.dart';
 
 part "cmdline/Application.dart";
 part "cmdline/Config.dart";
