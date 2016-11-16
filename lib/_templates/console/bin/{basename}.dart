3f58daf07e3ab87fbc669a8bc0acb29f097bea8c
Mike Mitterer <office@mikemitterer.at>
2016-11-16 16:09:17 +0100

    doc: Strong-Mode support

--- lib/_templates/console/bin/{basename}.dart
+++ lib/_templates/console/bin/{basename}.dart
@@ -1,6 +1,6 @@
 import 'package:<%= basename %>/<%= basename %>.dart';
 
-void main(List<String> arguments) {
+void main(final List<String> arguments) {
     final Application application = new Application();
     application.run( arguments );
 }
